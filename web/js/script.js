6d2c83231e8709931b70236d8ea5d35e3a52599b
josiane <josi-gomes05@hotmail.com>
2021-08-04 16:51:25 -0300

    aula 5.3.2 plugins

---
+++ web/js/script.js
@@ -0,0 +1,12 @@
+if(window.SimpleSlide){
+new SimpleSlide({
+  slide:"quote", //nome atribuido 
+  time:5000 //tempo de transicao dos slide (5000 sao 5segundos)
+})
+
+new SimpleSlide({
+  slide:"portfolio", 
+  time:5000,
+  nav: true
+})
+}
