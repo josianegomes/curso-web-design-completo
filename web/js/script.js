978b88e9251c5f46e694c80428543ca19290ab4d
josiane <josi-gomes05@hotmail.com>
2021-08-11 12:30:16 -0300

    php

--- web/js/script.js
+++ web/js/script.js
@@ -14,3 +14,12 @@ new SimpleSlide({
 if(window.SimpleAnime){ //fazer verificacao so vai funcionar se o SimpleAnime existir
 new SimpleAnime();
 }
+
+if(window.SimpleAnime){
+new SimpleForm({
+  form: ".formphp", // seletor do formulário
+  button: "#enviar", // seletor do botão
+  erro: "<div id='form-erro'><h2>Erro no envio</h2><p>Um erro ocorreu, tente para o email support@global-products-sell.com.</p></div>", // mensagem de erro
+  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+});
+}
